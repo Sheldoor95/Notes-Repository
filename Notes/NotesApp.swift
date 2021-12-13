294da27df59bf42184cb6285528ab55a8f8e9ae3
Sheldoor95 <jack_95@outlook.it>
2021-12-13 17:46:32 +0100

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Gian Marco Fantini on 13/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
